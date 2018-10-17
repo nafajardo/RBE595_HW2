16df695b206acf1a96fd4c10639b7a0d643c02b9
Nicholas Fajardo <nafajardo@wpi.edu>
2018-10-17 14:46:17 -0400

    Finished FwKin
    testing still needs to be done

--- Wrist.m
+++ Wrist.m
@@ -4,14 +4,12 @@
 classdef Wrist
     
     % Inner and Outer tube diameter
-    % Can only be set on construction, but can be read by all
     properties (GetAccess = public, SetAccess = immutable)
         InnerDiameter
         OuterDiameter
     end
     
     % Notches in the wrist
-    % Can only be set from within the class, but can be read by all
     properties (GetAccess = public, SetAccess = private)
         notches
         baseLength
@@ -23,38 +21,94 @@ classdef Wrist
         function obj = Wrist(innerDiameter, outerDiameter)
             obj.InnerDiameter = innerDiameter;
             obj.OuterDiameter = outerDiameter;
+            obj.baseLength = 0;
+            obj.notches = [];
         end
         
+        % Appends a notch to the wrist design
+        % Calculations depend on notch order being top down (tip to start)
         function obj = addNotch(obj, notch)
-            %addNotch takes in a notch and adds it to the wrist
             if class(notch) ~= "Notch"
                 error("Notch input must be an instance of a Notch class")
             end
+            
             obj.notches = [obj.notches, notch];
         end
         
+        % Sets a base length if appicable
         function obj = setBaseLength(obj, baseLength)
             obj.baseLength = baseLength;
         end
         
-        function obj = FwKin(obj, q)
-            % FwKin calculates the set of forward kinematics transformation
-            % matrices needed to represent the wrist
-            % q is the set of actuator variables
+        % Calculates the transformation matrices needed for the wrist using
+        % Frenet Serret frames, and assumes all notches are positioned the
+        % same (all notches have an orientation = 0 radians)
+        function transMatrices = FwKin(obj, q)
+            % Extracts the actuator variables
             deltaL = q(1, 1);
             alpha = q(1, 2);
             tau = q(1, 3);
             
             transMatrices = [];
             
-            % Add the transformation matrix of a base length translation
-            % among the Z
-            tBase = [0, 0, 0, 0;
-                     0, 0, 0, 0;
-                     0, 0, 0, obj.baseLength;
+            % Calculates the transformation matrix for the base translation
+            tBase = [cos(alpha), -sin(alpha), 0, 0;
+                     sin(alpha),  cos(alpha), 0, 0;
+                     0, 0, 1, obj.baseLength + tau;
                      0, 0, 0, 1];
             
             transMatrices = [transMatrices, tBase];
+            
+            % neutral bending plane displacement from notch frame
+            y = nbpLoc(obj);
+            
+            % Populates the transMatrices list with the matrices
+            for index = size(obj.notches):-1:1
+                % Grabbing notch (assumes list order of tip to beginning)
+                notch = obj.notches(1, index);
+                
+                % Converting actuator space to configuration space
+                k = deltaL/(notch.Height * ((obj.InnerDiameter/2) + y) - deltaL * y);
+                s = notch.Height/(1 + y * k);
+                
+                % Calculating reference frame with gathered variables
+                T_Notch = [1, 0, 0, 0;
+                           0, cos(k*s), -sin(k*s), (cos(k*s) - 1)/k;
+                           0, sin(k*s),  cos(k*s), sin(k*s)/k;
+                           0, 0, 0, 1];
+                
+                transMatrices = [transMatrices, T_Notch];
+                
+                if notch.distanceFromPrev ~= 0
+                    % Appends a translation amongst the Z axis
+                    T_ToNext = [1, 0, 0, 0;
+                                0, 1, 0, 0;
+                                0, 0, 1, notch.distanceFromPrev;
+                                0, 0, 0, 1]
+                    
+                    transMatrices = [transMatrices, T_ToNext];
+                end
+            end
+            
+            disp("Total Frames for FwKin of wrist found: " + size(transMatrices))
+        end
+        
+        function T_Matrix = fsTransMatrix(rotZ, curvature, length)
+            T_Matrix = [cos(rotZ)*cos(curvature*length), -sin(rotZ), cos(rotZ)*sin(curvature*length), (cos(rotZ)*(1-cos(curvature*length)))/(curvature);
+                        sin(rotZ)*cos(curvature*length),  cos(rotZ), sin(rotZ)*sin(curvature*length), (sin(rotZ)*(1-cos(curvature*length)))/(curvature);
+                        -sin(curvature*length),                   0, cos(curvature*length),           (sin(curvature*length))/(curvature);
+                        0, 0, 0, 1];
+        end
+        
+        function y = nbpLoc(obj)
+            phi_o = 2 * acos(((innerDiameter/2 + outerDiameter/2) - (obj.OuterDiameter))/(obj.OuterDiameter/2));
+            phi_i = 2 * acos(((innerDiameter/2 + outerDiameter/2) - (obj.InnerDiameter))/(obj.InnerDiameter/2));
+            y_o = (4 * (outerDiameter/2) * ((sin(.5 * phi_o))^3))/(3 * (phi_o - sin(phi_o)));
+            y_i = (4 * (innerDiameter/2) * ((sin(.5 * phi_i))^3))/(3 * (phi_i - sin(phi_i)));
+            A_o = (((outerDiameter/2)^2) * (phi_o - sin(phi_o)))/2;
+            A_i = (((innerDiameter/2)^2) * (phi_i - sin(phi_i)))/2;
+            
+            y = (y_o * A_o - y_i * A_i)/(A_o - A_i);
         end
     end
 end
--- fsTransMatrix.m
+++
@@ -1,9 +0,0 @@
-function T_Matrix = fsTransMatrix(rotZ, curvature, length)
-            % fsTransMatrix takes in the variables needed for a Frenet
-            % Serret Transformation matrix, and returns the Homogeneous
-            % Transformation Matrix using the given parameters
-            T_Matrix = [cos(rotZ)*cos(curvature*length), -sin(rotZ), cos(rotZ)*sin(curvature*length), (cos(rotZ)*(1-cos(curvature*length)))/(curvature);
-                        sin(rotZ)*cos(curvature*length),  cos(rotZ), sin(rotZ)*sin(curvature*length), (sin(rotZ)*(1-cos(curvature*length)))/(curvature);
-                        -sin(curvature*length),                   0, cos(curvature*length),           (sin(curvature*length))/(curvature);
-                        0, 0, 0, 1];
-        end
